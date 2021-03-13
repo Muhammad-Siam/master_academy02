7a96a7eb94b3800564caf032158ef884f80a0b40
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-03-13 14:13:56 +0600

    web form handling and sql done

--- Lecture_18/main.go
+++ Lecture_18/main.go
@@ -1,30 +1,57 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"html/template"
 	"net/http"
-	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/Features", Features)
-	http.HandleFunc("/Docs", Docs)
+var db *sql.DB
+var err error
 
-	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./assets"))))
-	http.ListenAndServe("localhost:8888", nil)
+func init() {
+
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err = sql.Open("mysql", "root:xioami14560@tcp(127.0.0.1:3306)/hosting_db")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	//defer db.Close()
+	fmt.Println("db connection successful")
+}
+
+func main() {
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/request", request)
+	http.HandleFunc("/features", features)
+	http.HandleFunc("/docs", docs)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":8888", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+
 	ptmp, err := template.ParseFiles("template/base.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp.Execute(w, nil)
-	//fmt.Fprintf(w, "Welcome to siam")
+	//fmt.Fprintf(w, `welcome`)
 }
-func Features(w http.ResponseWriter, r *http.Request) {
+
+func features(w http.ResponseWriter, r *http.Request) {
 
 	ptmp, err := template.ParseFiles("template/base.gohtml")
 	if err != nil {
@@ -32,27 +59,53 @@ func Features(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ptmp, err = ptmp.ParseFiles("wpage/features.gohtml")
-
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	ptmp.Execute(w, nil)
-	//fmt.Fprintf(w, "Welcome to siam")
+	//fmt.Fprintf(w, `welcome`)
 }
 
-func Docs(w http.ResponseWriter, r *http.Request) {
+func docs(w http.ResponseWriter, r *http.Request) {
+
 	ptmp, err := template.ParseFiles("template/base.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp, err = ptmp.ParseFiles("wpage/docs.gohtml")
-
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	ptmp.Execute(w, nil)
-	//fmt.Fprintf(w, "Welcome to siam")
+	//fmt.Fprintf(w, `welcome`)
+}
+
+func request(w http.ResponseWriter, r *http.Request) {
+
+	//method-1
+	name := r.FormValue("name")
+	company := r.FormValue("company")
+	email := r.FormValue("email")
+
+	// fmt.Println(name, company, email)
+	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
+
+	//method-2
+	// r.ParseForm()
+	// for key, val := range r.Form {
+	// 	fmt.Println(key, val)
+	// }
+
+	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
+	sql := fmt.Sprintf(qs, name, company, email)
+	//fmt.Println(sql)
+	insert, err := db.Query(sql)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+
+	fmt.Fprintf(w, `OK`)
 }
