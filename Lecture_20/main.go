b492f01958374057e3e4a104d7644dba44060bc4
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-03-17 17:50:41 +0600

    fORM VIA POSTMAN AJAX AND JQUEARY AMD MCB DONE

--- Lecture_20/main.go
+++ Lecture_20/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/mateors/mcb"
 )
 
-var db *sql.DB
+//var db *sql.DB
+var db *mcb.DB
 var err error
 
 func init() {
@@ -17,23 +18,33 @@ func init() {
 	// Open up our database connection.
 	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called testDb
-	db, err = sql.Open("mysql", "root:xioami14560@tcp(127.0.0.1:3306)/hosting_db")
+	//db, err = sql.Open("mysql", "root:test123@tcp(127.0.0.1:3306)/hosting_db")
 
 	// if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
-	}
-
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
 	// defer the close till after the main function has finished
 	// executing
 	//defer db.Close()
-	fmt.Println("db connection successful")
+	//fmt.Println("db connection successful")
+
+	//couchbase connection block
+	db = mcb.Connect("localhost", "Siam", "xioami14560")
+	res, err := db.Ping()
+	if err != nil {
+
+		fmt.Println(res)
+		os.Exit(1)
+	}
+	fmt.Println(res, err)
 }
 
 func main() {
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/request", request)
+	//http.HandleFunc("/requestc", requestc)
 	http.HandleFunc("/features", features)
 	http.HandleFunc("/docs", docs)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
@@ -82,30 +93,68 @@ func docs(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, `welcome`)
 }
 
+// func request(w http.ResponseWriter, r *http.Request) {
+
+// 	//method-1
+// 	name := r.FormValue("name")
+// 	company := r.FormValue("company")
+// 	email := r.FormValue("email")
+
+// 	// fmt.Println(name, company, email)
+// 	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
+
+// 	//method-2
+// 	// r.ParseForm()
+// 	// for key, val := range r.Form {
+// 	// 	fmt.Println(key, val)
+// 	// }
+
+// 	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
+// 	sql := fmt.Sprintf(qs, name, company, email)
+// 	//fmt.Println(sql)
+// 	insert, err := db.Query(sql)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+// 	defer insert.Close()
+
+// 	fmt.Fprintf(w, `OK`)
+// }
+
+//like mysql table schema
+type RequestTable struct {
+	ID      string `json:"aid"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Company string `json:"company"`
+	Type    string `json:"type"`
+	Status  int    `json:"status"`
+}
+
 func request(w http.ResponseWriter, r *http.Request) {
 
 	//method-1
-	name := r.FormValue("name")
-	company := r.FormValue("company")
-	email := r.FormValue("email")
+	// name := r.FormValue("name")
+	// company := r.FormValue("company")
+	// email := r.FormValue("email")
 
 	// fmt.Println(name, company, email)
 	// fmt.Fprintf(w, `received %s %s %s`, name, company, email) //response
 
 	//method-2
-	// r.ParseForm()
-	// for key, val := range r.Form {
-	// 	fmt.Println(key, val)
-	// }
-
-	qs := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1');"
-	sql := fmt.Sprintf(qs, name, company, email)
-	//fmt.Println(sql)
-	insert, err := db.Query(sql)
-	if err != nil {
-		panic(err.Error())
+	r.ParseForm()
+	for key, val := range r.Form {
+		fmt.Println(key, val)
 	}
-	defer insert.Close()
+
+	var reqTable RequestTable
+
+	r.Form.Add("bucket", "master_academy")
+	r.Form.Add("aid", "request::7") //we will update later
+	r.Form.Add("type", "request")
+	r.Form.Add("status", "1")
+	pRes := db.Insert(r.Form, &reqTable)
+	fmt.Println(pRes.Status, pRes.Errors)
 
 	fmt.Fprintf(w, `OK`)
 }
